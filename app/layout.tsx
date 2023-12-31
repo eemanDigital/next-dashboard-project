f31a3a78261a2100743505b8ccecc773475bbf83
eemanDigital <eemandigitalconcept@gamil.com>
2023-12-31 21:09:29 +0100

    first push

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      {/* Here, you're also adding the Tailwind antialiased.class which smooths out the font. It's not necessary to use this class, but it adds a nice touch. */}
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
